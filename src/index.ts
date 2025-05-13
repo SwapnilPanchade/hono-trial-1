74c9e621957dbe8d1eab0e86db691f6942298b11
SwapnilPanchade <sspanchade113@gmail.com>
2025-05-13 22:02:04 +0530

    new authentication part added in the project here

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,18 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
+const authMiddleware = (c:any , next : any) => {
+  if(c.req.header("Authorizatio")){
+    
+  }
+}
+
+app.get('/', async (c) => {
+  const body = await c.req.json();
+  console.log(body);
+  console.log(c.req.header("Authorization"));
+  console.log(c.req.query("param"));
+  
   return c.text('Hello Hono!')
 })
 
