38bd991d4439d52e117720861ffb98564fef9810
SwapnilPanchade <sspanchade113@gmail.com>
2025-05-14 11:21:59 +0530

    updated the auth

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,23 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-const authMiddleware = (c:any , next : any) => {
-  if(c.req.header("Authorizatio")){
-    
+const authMiddleware = async (c: any, next: any) => {
+  if (c.req.header("Authorization")) {
+    console.log("The user is authenticated");
+    await next();
+  } else {
+    return c.text("You don't have access to this part yet");
   }
-}
+};
 
-app.get('/', async (c) => {
+app.get("/", authMiddleware, async (c) => {
   const body = await c.req.json();
   console.log(body);
   console.log(c.req.header("Authorization"));
   console.log(c.req.query("param"));
-  
-  return c.text('Hello Hono!')
-})
 
-export default app
+  return c.text("Hello Hono!");
+});
+
+export default app;
